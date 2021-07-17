a6a609826834dd292771ae977c07ed42fa4abab4
livek8888 <livek8888@gmail.com>
2021-07-17 15:34:46 +0900

    include 수정

--- public/apple_login.php
+++ public/apple_login.php
@@ -1,9 +1,12 @@
+<?php
+include_once '../settings.php';
+ ?>
 <html>
     <head>
         <script type="text/javascript" src="https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js"></script>
-        <meta name="appleid-signin-client-id" content="tv.livek.newrose2">
+        <meta name="appleid-signin-client-id" content="<?=$client_id?>">
         <meta name="appleid-signin-scope" content="email">
-        <meta name="appleid-signin-redirect-uri" content="https://newrose2.livek.tv/apple_login_callback.php">
+        <meta name="appleid-signin-redirect-uri" content="<?=$redirect_uri?>">
         <meta name="appleid-signin-state" content="Test">
         <meta name="appleid-signin-use-popup" content="false"> <!-- or false defaults to false -->
     </head>
